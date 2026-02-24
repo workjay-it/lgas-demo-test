c7af6cde51797b82daa8f03d12da1032d5c29d79
Jay N <workjay01@gmail.com>
2026-02-24 17:04:01 +0530

    Update lgas1.py

--- lgas1.py
+++ lgas1.py
@@ -215,12 +215,13 @@ st.markdown("---")
 last_time = st.session_state["last_refresh"]
 footer_text = f"""
 <div style="text-align: center; color: grey; font-size: 0.85em; font-family: sans-serif;">
-    <p><b> Developed by </b> Dev Team of KWS </p>
+    <p><b> Developed by </b> KWS Dev Team Member Jay </p>
     <p style="color: #007bff;"><b>Last Refresh:</b> {last_time} IST</p>
     <p> Cylinder Management System v1.2</p>
 </div>
 """
 st.markdown(footer_text, unsafe_allow_html=True)
+
 
 
 
